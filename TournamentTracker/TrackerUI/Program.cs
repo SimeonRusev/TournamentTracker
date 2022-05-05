13eeb62f031290c1f3b5f44d23305dd52709e840
SimeonRusev <s.rusev1@gmail.com>
2022-05-05 22:56:40 +0300

    progress on SaveTournamentRounds

--- TournamentTracker/TrackerUI/Program.cs
+++ TournamentTracker/TrackerUI/Program.cs
@@ -19,7 +19,7 @@ namespace TrackerUI
             Application.SetCompatibleTextRenderingDefault(false);
 
             /// Initialize the database connections
-            TrackerLibrary.GlobalConfig.InitializeConnections(TrackerLibrary.DatabaseType.TextFile);
+            TrackerLibrary.GlobalConfig.InitializeConnections(TrackerLibrary.DatabaseType.Sql);
             Application.Run(new CreateTournamentForm());
 
 
